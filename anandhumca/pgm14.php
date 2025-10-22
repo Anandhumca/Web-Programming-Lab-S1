6bb082c84bfee4f6aa8e986b0ab440ecd63891b4
Anandhumca <chn25mca042@ceconline.edu>
2025-10-22 15:31:26 +0530

    php
    first 2 php

---
+++ anandhumca/pgm14.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<title>ODD OR EVEN</title>
+	<head>
+	<body>
+		<h1>Odd Even Checker</h1>
+		<form method="GET" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			Enter a number:<input type="text" name="number">
+			<input type="submit" value="Check">
+		</form>
+		<?php
+			if($_SERVER["REQUEST_METHOD"]=="GET")
+			{
+				if(isset($_GET["number"]))
+				{
+					$number=$_GET["number"];
+					if(is_numeric($number))
+					{
+						if($number%2==0)
+						{	
+							echo "<p>{$number} is an even number.</p>";
+						}
+						else
+						{
+							echo "<p>{$number} is an odd number.</p>";
+						}
+					}
+					else
+					{
+						echo  "<p> Please enter a valid number.</p>";
+					}
+				}
+			}
+		?>
+	</body>
+</html>
